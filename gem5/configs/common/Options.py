66019abe18d4dfc92117536db9a1347e4d5ba7c6
xxrjun <rjun0729@gmail.com>
2024-06-16 18:16:58 +0000

    set the defaulve value of option --l3_assoc to 32

--- gem5/configs/common/Options.py
+++ gem5/configs/common/Options.py
@@ -109,7 +109,7 @@ def addNoISAOptions(parser):
     parser.add_option("--l1d_assoc", type="int", default=2)
     parser.add_option("--l1i_assoc", type="int", default=2)
     parser.add_option("--l2_assoc", type="int", default=8)
-    parser.add_option("--l3_assoc", type="int", default=16)
+    parser.add_option("--l3_assoc", type="int", default=32)
     parser.add_option("--cacheline_size", type="int", default=64)
 
     # Enable Ruby

0349e8c4fe82066ef488f2b183d54a1621dbc64d
xxrjun <rjun0729@gmail.com>
2024-06-19 17:40:46 +0000

    update several output

--- scripts/task3_quicksort_benchmark.sh
+++ scripts/task3_quicksort_benchmark.sh
@@ -56,6 +56,123 @@ echo $cmd > ../out/quicksort-full-way/cmd.txt
 mv m5out/stats.txt ../out/quicksort-full-way/stats.txt
 mv m5out/config.ini ../out/quicksort-full-way/config.ini
 
+# ==============================================================
+# 2-way associative with smaller L3 cache size: 128kB
+echo "Running 2-way associative cache with smaller L3 cache size..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=128kB --l3_assoc=2 \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+mkdir -p ../out/quicksort-2-way-small-l3-128kB
+eval $cmd > ../out/quicksort-2-way-small-l3-128kB/log.txt
+echo $cmd > ../out/quicksort-2-way-small-l3-128kB/cmd.txt
+mv m5out/stats.txt ../out/quicksort-2-way-small-l3-128kB/stats.txt
+mv m5out/config.ini ../out/quicksort-2-way-small-l3-128kB/config.ini
+
+# ==============================================================
+# Full-way associative with smaller L3 cache size: 128kB
+echo "Running full-way associative cache with smaller L3 cache size..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=128kB --l3_assoc=16 \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+
+mkdir -p ../out/quicksort-full-way-small-l3-128kB
+eval $cmd > ../out/quicksort-full-way-small-l3-128kB/log.txt
+echo $cmd > ../out/quicksort-full-way-small-l3-128kB/cmd.txt
+mv m5out/stats.txt ../out/quicksort-full-way-small-l3-128kB/stats.txt
+mv m5out/config.ini ../out/quicksort-full-way-small-l3-128kB/config.ini
+
+# ==============================================================
+# 2-way associative with smaller L3 cache size, replacement policy: FIFO
+echo "Running 2-way associative cache with smaller L3 cache size and FIFO replacement policy..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=128kB --l3_assoc=2 --l3_replacement_policy=FIFO \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+mkdir -p ../out/quicksort-2-way-small-l3-128kB-FIFO
+eval $cmd > ../out/quicksort-2-way-small-l3-128kB-FIFO/log.txt
+echo $cmd > ../out/quicksort-2-way-small-l3-128kB-FIFO/cmd.txt
+mv m5out/stats.txt ../out/quicksort-2-way-small-l3-128kB-FIFO/stats.txt
+mv m5out/config.ini ../out/quicksort-2-way-small-l3-128kB-FIFO/config.ini
+
+# ==============================================================
+# Full-way associative with smaller L3 cache size, replacement policy: FIFO
+echo "Running full-way associative cache with smaller L3 cache size and FIFO replacement policy..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=128kB --l3_assoc=16 --l3_replacement_policy=FIFO \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+
+mkdir -p ../out/quicksort-full-way-small-l3-128kB-FIFO
+eval $cmd > ../out/quicksort-full-way-small-l3-128kB-FIFO/log.txt
+echo $cmd > ../out/quicksort-full-way-small-l3-128kB-FIFO/cmd.txt
+mv m5out/stats.txt ../out/quicksort-full-way-small-l3-128kB-FIFO/stats.txt
+mv m5out/config.ini ../out/quicksort-full-way-small-l3-128kB-FIFO/config.ini
+
+# ==============================================================
+# 2-way associative with much smaller L3 cache size: 4kB, replacement policy: default(LRU)
+echo "Running 2-way associative cache with much smaller L3 cache size and default replacement policy..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=4kB --l3_assoc=2 \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+
+mkdir -p ../out/quicksort-2-way-small-l3-4kB
+eval $cmd > ../out/quicksort-2-way-small-l3-4kB/log.txt
+echo $cmd > ../out/quicksort-2-way-small-l3-4kB/cmd.txt
+mv m5out/stats.txt ../out/quicksort-2-way-small-l3-4kB/stats.txt
+mv m5out/config.ini ../out/quicksort-2-way-small-l3-4kB/config.ini
+
+# ==============================================================
+# Full-way associative with much smaller L3 cache size: 4kB, replacement policy: default(LRU)
+echo "Running full-way associative cache with much smaller L3 cache size and default replacement policy..."
+cmd="./build/X86/gem5.opt $CONFIG_SCRIPT \
+        -c $QUICKSORT_BINARY \
+        --cpu-type=$CPU_TYPE \
+        --caches \
+        --l1d_size=$L1d_SIZE --l1i_size=$L1i_SIZE \
+        --l2cache --l2_size=$L2_SIZE \
+        --l3cache --l3_size=4kB --l3_assoc=16 \
+        --mem-type=$MEM_TYPE \
+        --nvmain-config=$NVMAIN_CONFIG
+    "
+
+mkdir -p ../out/quicksort-full-way-small-l3-4kB
+eval $cmd > ../out/quicksort-full-way-small-l3-4kB/log.txt
+echo $cmd > ../out/quicksort-full-way-small-l3-4kB/cmd.txt
+mv m5out/stats.txt ../out/quicksort-full-way-small-l3-4kB/stats.txt
+mv m5out/config.ini ../out/quicksort-full-way-small-l3-4kB/config.ini
 
 # ==============================================================
 echo "Results for 2-way associative cache:"
@@ -63,3 +180,21 @@ grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "syst
 
 echo "Results for full-way associative cache:"
 grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-full-way/stats.txt
+
+echo "Results for 2-way associative cache with smaller L3 cache size:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-2-way-small-l3-128kB/stats.txt
+
+echo "Results for full-way associative cache with smaller L3 cache size:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-full-way-small-l3-128kB/stats.txt
+
+echo "Results for 2-way associative cache with smaller L3 cache size and FIFO replacement policy:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-2-way-small-l3-128kB-FIFO/stats.txt
+
+echo "Results for full-way associative cache with smaller L3 cache size and FIFO replacement policy:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-full-way-small-l3-128kB-FIFO/stats.txt
+
+echo "Results for 2-way associative cache with much smaller L3 cache size and default replacement policy:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-2-way-small-l3-4kB/stats.txt
+
+echo "Results for full-way associative cache with much smaller L3 cache size and default replacement policy:"
+grep -e "sim_seconds" -e "sim_ticks" -e "system.l3.overall_hits::total" -e "system.l3.overall_misses::total" -e "system.l3.overall_miss_rate::total" ../out/quicksort-full-way-small-l3-4kB/stats.txt
